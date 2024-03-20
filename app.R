87a87d9a4f250e9116a443e6b5945d88d52aabcc
Xinge Cai <99489792+caixinge1@users.noreply.github.com>
2024-03-20 00:29:19 -0400

    3/19
    Works for NCAA and UNC_ACC

--- app.R
+++ app.R
@@ -1,9 +1,7 @@
-# app.R
 library(shiny)
 
-source("global.R")
+# Assuming ui.R and server.R are in the same directory as app.R
 source("ui.R")
 source("server.R")
-
 
 shinyApp(ui = ui, server = server)
